05a7f8ca085c976f9599348c172c8c4326c7eefa
boodah <boodah21@protonmail.com>
2018-12-17 05:00:37 -0500

    Se crea el panel de reportes. Sin problemas. Prox el panel inter_user.

---
+++ src/paneles/Panel_inter_user.java
@@ -0,0 +1,19 @@
+package paneles;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+public class Panel_inter_user extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public Panel_inter_user() {
+		this.setBounds(0, 0, 1035, 582);
+		this.setBackground(new Color(16,32,39));
+		setLayout(null);
+		
+	}
+
+}
